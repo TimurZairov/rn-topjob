104c2560d3ece3f23e68b857174242c29c8bbc10
Timur <timur@Timur.local>
2024-03-03 03:13:20 +0500

    tabNavigation

--- src/assets/icons/MainTab.tsx
+++ src/assets/icons/MainTab.tsx
@@ -2,7 +2,11 @@ import {View} from 'react-native';
 import React from 'react';
 import {Path, Svg} from 'react-native-svg';
 
-const MainTab = () => {
+interface IMainTab {
+  color?: string;
+}
+
+const MainTab = ({color}: IMainTab) => {
   return (
     <View>
       <Svg width="28" height="28" viewBox="0 0 28 28" fill="none">
@@ -10,7 +14,7 @@ const MainTab = () => {
           fill-rule="evenodd"
           clip-rule="evenodd"
           d="M22.1552 23.3334H18.6669V15.1667C18.6669 14.5215 18.1442 14 17.5002 14H10.5002C9.85508 14 9.33358 14.5215 9.33358 15.1667V23.3334H5.83358L5.84058 13.5135L13.9979 5.17071L22.1669 13.5614L22.1552 23.3334ZM11.6669 23.3334H16.3336V16.3334H11.6669V23.3334ZM23.8282 11.8825L14.8344 2.68453C14.3946 2.23536 13.6059 2.23536 13.1661 2.68453L4.17108 11.8837C3.74524 12.3212 3.50024 12.9325 3.50024 13.5614V23.3334C3.50024 24.6202 4.48841 25.6667 5.70291 25.6667H10.5002H17.5002H22.2964C23.5109 25.6667 24.5002 24.6202 24.5002 23.3334V13.5614C24.5002 12.9325 24.2552 12.3212 23.8282 11.8825Z"
-          fill="#888888"
+          fill={color}
         />
       </Svg>
     </View>
--- src/navigation/TabNavigation.tsx
+++ src/navigation/TabNavigation.tsx
@@ -1,21 +1,84 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
+//
 import MainScreen from '../screens/MainScreen/MainScreen';
 import VacanciesScreen from '../screens/VacanciesScreen/VacanciesScreen';
 import ServicesScreen from '../screens/ServicesScree/ServicesScreen';
 import TaskScreen from '../screens/TaskScreen/TaskScreen';
 import ProfileScreen from '../screens/ProfileScreen/ProfileScreen';
+import MainTab from '../assets/icons/MainTab';
+import {COLORS} from '../theme/theme';
+import TaskTab from '../assets/icons/TaskTab';
+import ProfileTab from '../assets/icons/ProfileTab';
+import ServiceTab from '../assets/icons/ServiceTab';
+import VacancyTab from '../assets/icons/VacancyTab';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}} initialRouteName="Main">
-      <Tab.Screen name="Vacancy" component={VacanciesScreen} />
-      <Tab.Screen name="Service" component={ServicesScreen} />
-      <Tab.Screen name="Main" component={MainScreen} />
-      <Tab.Screen name="Task" component={TaskScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.mainOrange,
+        tabBarInactiveTintColor: '#888888',
+      }}
+      initialRouteName="Main">
+      <Tab.Screen
+        name="Vacancy"
+        component={VacanciesScreen}
+        options={{
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({color}) => {
+            return <VacancyTab color={color} />;
+          },
+          tabBarLabel: 'Вакансии',
+        }}
+      />
+      <Tab.Screen
+        name="Service"
+        component={ServicesScreen}
+        options={{
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({color}) => {
+            return <ServiceTab color={color} />;
+          },
+          tabBarLabel: 'Услуги',
+        }}
+      />
+      <Tab.Screen
+        name="Main"
+        component={MainScreen}
+        options={{
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({color}) => {
+            return <MainTab color={color} />;
+          },
+          tabBarLabel: 'Главная',
+        }}
+      />
+      <Tab.Screen
+        name="Task"
+        component={TaskScreen}
+        options={{
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({color}) => {
+            return <TaskTab color={color} />;
+          },
+          tabBarLabel: 'Задачи',
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({color}) => {
+            return <ProfileTab color={color} />;
+          },
+          tabBarLabel: 'Профиль',
+        }}
+      />
     </Tab.Navigator>
   );
 };
